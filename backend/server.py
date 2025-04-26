cbea1afd90f4533333802e2ac231cee480697d19
Pen-Gun <namshang715@gmail.com>
2025-04-27 00:16:21 +0545

    for backend
    minor changes

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from routes.youtube import search_yt  # Import the search blueprint
@@ -9,4 +10,6 @@ CORS(app)
 app.register_blueprint(search_yt)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)  # Listen on all IPs (publicly accessible)
